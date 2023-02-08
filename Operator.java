ca9373403828c23da55963eb01f2a928c8f5bf05
Mbaliii <mbalikhumalo408@gmail.com>
2023-02-08 20:22:55 +0200

    Operator

---
+++ Operator.java
@@ -0,0 +1,19 @@
+package Sl;
+
+import java.util.Scanner;
+
+//Arithematic operators
+//variables x, y, z and answer
+// Setting the variables to integers
+//variable answer combining all values and concatinating them towards a string
+public class Operator {
+    public static void main(String[] args){
+        int x, y, z , answer;
+        x = 45;
+        y = 89;
+        z = 87;
+
+        answer = x + y / z;
+        System.out.println("Answer is " + answer);
+    }
+}
